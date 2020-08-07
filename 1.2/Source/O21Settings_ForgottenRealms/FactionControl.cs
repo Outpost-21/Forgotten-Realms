9d794d9d944b469440895ae2b0313e6446a0cc78
neronix17 <39186138+neronix17@users.noreply.github.com>
2020-08-07 12:59:11 +0100

    Initial commit

---
+++ 1.2/Source/O21Settings_ForgottenRealms/FactionControl.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Settings_ForgottenRealms
+{
+    [StaticConstructorOnStartup]
+    public static class FactionControl
+    {
+        static FactionControl()
+        {
+            ForgottenRealmsSettings settings = ForgottenRealmsMod.mod.settings;
+
+            if (!settings.faction_elfDark)
+            {
+                ForgottenFactionsDefOf.O21_DarkElfKingdoms.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_DarkElfKingdoms.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_elfMoon)
+            {
+                ForgottenFactionsDefOf.O21_MoonElfKingdoms.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_MoonElfKingdoms.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_elfSun)
+            {
+                ForgottenFactionsDefOf.O21_SunElfKingdoms.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_SunElfKingdoms.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_elfWood)
+            {
+                ForgottenFactionsDefOf.O21_WoodElfKingdoms.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_WoodElfKingdoms.maxCountAtGameStart = 0;
+            }
+
+            if (!settings.faction_gith)
+            {
+                ForgottenFactionsDefOf.O21_GithTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_GithTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_goblin)
+            {
+                ForgottenFactionsDefOf.O21_GoblinTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_GoblinTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_hobgoblin)
+            {
+                ForgottenFactionsDefOf.O21_HobgoblinTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_HobgoblinTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_illithid)
+            {
+                ForgottenFactionsDefOf.O21_IllithidTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_IllithidTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_kobold)
+            {
+                ForgottenFactionsDefOf.O21_KoboldTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_KoboldTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_orc)
+            {
+                ForgottenFactionsDefOf.O21_OrcTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_OrcTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_tiefling)
+            {
+                ForgottenFactionsDefOf.O21_TieflingTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_TieflingTribes.maxCountAtGameStart = 0;
+            }
+            if (!settings.faction_warforged)
+            {
+                ForgottenFactionsDefOf.O21_WarforgedTribes.requiredCountAtGameStart = 0;
+                ForgottenFactionsDefOf.O21_WarforgedTribes.maxCountAtGameStart = 0;
+            }
+        }
+    }
+}
---
+++ 1.2/Source/O21Settings_ForgottenRealms/ForgottenRealmsMod.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Settings_ForgottenRealms
+{
+    public class ForgottenRealmsMod : Mod
+    {
+        public ForgottenRealmsSettings settings;
+        public static ForgottenRealmsMod mod;
+
+        public ForgottenRealmsMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<ForgottenRealmsSettings>();
+            mod = this;
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Faction Settings - Changing any of these requires a restart to take effect.");
+            listingStandard.CheckboxLabeled("Dark Elves", ref settings.faction_elfDark, "Dark Elf Faction");
+            listingStandard.CheckboxLabeled("Moon Elves", ref settings.faction_elfMoon, "Moon Elf Faction");
+            listingStandard.CheckboxLabeled("Sun Elves", ref settings.faction_elfSun, "Sun Elf Faction");
+            listingStandard.CheckboxLabeled("Wood Elves", ref settings.faction_elfWood, "Wood Elf Faction");
+            listingStandard.CheckboxLabeled("Gith", ref settings.faction_gith, "Gith Faction");
+            listingStandard.CheckboxLabeled("Goblin", ref settings.faction_goblin, "Goblin Faction");
+            listingStandard.CheckboxLabeled("Hobgoblin", ref settings.faction_hobgoblin, "Hobgoblin Faction");
+            listingStandard.CheckboxLabeled("Illithid", ref settings.faction_illithid, "Illithid Faction");
+            listingStandard.CheckboxLabeled("Kobold", ref settings.faction_kobold, "Kobold Faction");
+            listingStandard.CheckboxLabeled("Orc", ref settings.faction_orc, "Orc Faction");
+            listingStandard.CheckboxLabeled("Tiefling", ref settings.faction_tiefling, "Tiefling Faction");
+            listingStandard.CheckboxLabeled("Warforged", ref settings.faction_warforged, "Warforged Faction");
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Forgotten Realms";
+        }
+    }
+
+    public class ForgottenRealmsSettings : ModSettings
+    {
+        public bool faction_elfDark = true;
+        public bool faction_elfMoon = true;
+        public bool faction_elfSun = true;
+        public bool faction_elfWood = true;
+
+        public bool faction_gith = true;
+        public bool faction_goblin = true;
+        public bool faction_hobgoblin = true;
+        public bool faction_illithid = true;
+        public bool faction_kobold = true;
+        public bool faction_orc = true;
+        public bool faction_tiefling = true;
+        public bool faction_warforged = true;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref faction_elfDark, "faction_elfDark", true);
+            Scribe_Values.Look(ref faction_elfMoon, "faction_elfMoon", true);
+            Scribe_Values.Look(ref faction_elfSun, "faction_elfSun", true);
+            Scribe_Values.Look(ref faction_elfWood, "faction_elfWood", true);
+
+            Scribe_Values.Look(ref faction_gith, "faction_gith", true);
+            Scribe_Values.Look(ref faction_goblin, "faction_goblin", true);
+            Scribe_Values.Look(ref faction_hobgoblin, "faction_hobgoblin", true);
+            Scribe_Values.Look(ref faction_illithid, "faction_illithid", true);
+            Scribe_Values.Look(ref faction_kobold, "faction_kobold", true);
+            Scribe_Values.Look(ref faction_orc, "faction_orc", true);
+            Scribe_Values.Look(ref faction_tiefling, "faction_tiefling", true);
+            Scribe_Values.Look(ref faction_warforged, "faction_warforged", true);
+        }
+    }
+}
